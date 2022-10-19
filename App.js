5c6def4d1300d6e13f1a986519bc7d67adb2ece9
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-19 09:42:00 +0100

    remove web folder

---
+++ App.js
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
